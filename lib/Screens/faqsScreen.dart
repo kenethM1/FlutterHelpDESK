e1756401bfaed25b1d29ed0ac586e6852aa4bffe
L Jared Palma <l.jared.p31@gmail.com>
2021-06-19 10:48:44 -0600

    agregar y ver FAQs

--- lib/Screens/faqsScreen.dart
+++ lib/Screens/faqsScreen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterdesk/Models/faqs.dart';
-import 'package:flutterdesk/Providers/FaqsProvider.dart';
 import 'package:provider/provider.dart';
 
 
